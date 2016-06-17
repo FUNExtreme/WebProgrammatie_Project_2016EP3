6ddca53c87bb1c1263c4f8266b96fd3d29853cb3
FUNExtreme <robin.maenhaut.coder@gmail.com>
2016-06-17 17:44:01 +0200

    Project cleanup and restructuring

--- Source/JeugdlocatieBooking/JeugdlocatieBooking/Areas/HelpPage/ModelDescriptions/ComplexTypeModelDescription.cs
+++ Source/JeugdlocatieBooking/JeugdlocatieBooking/Areas/HelpPage/ModelDescriptions/ComplexTypeModelDescription.cs
@@ -1,6 +1,6 @@
 using System.Collections.ObjectModel;
 
-namespace WebApplication1.Areas.HelpPage.ModelDescriptions
+namespace JeugdlocatieBooking.Areas.HelpPage.ModelDescriptions
 {
     public class ComplexTypeModelDescription : ModelDescription
     {
--- Source/JeugdlocatieBooking/JeugdlocatieBooking/Areas/HelpPage/ModelDescriptions/EnumTypeModelDescription.cs
+++ Source/JeugdlocatieBooking/JeugdlocatieBooking/Areas/HelpPage/ModelDescriptions/EnumTypeModelDescription.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
-namespace WebApplication1.Areas.HelpPage.ModelDescriptions
+namespace JeugdlocatieBooking.Areas.HelpPage.ModelDescriptions
 {
     public class EnumTypeModelDescription : ModelDescription
     {
