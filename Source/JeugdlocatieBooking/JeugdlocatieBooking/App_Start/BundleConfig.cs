62e068a1a4769ee9e0018fb754a1e1b32c016e57
Robin Maenhaut <roma@adifo.be>
2016-07-28 11:06:30 +0200

    Cleanup

--- Source/JeugdlocatieBooking/JeugdlocatieBooking/App_Start/BundleConfig.cs
+++ Source/JeugdlocatieBooking/JeugdlocatieBooking/App_Start/BundleConfig.cs
@@ -5,15 +5,16 @@ namespace JeugdlocatieBooking
 {
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            /*
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Assets/js/bootstrap.js"));
+            */
 
-            bundles.Add(new StyleBundle("~/Assets/css").Include(
-                      "~/Assets/css/bootstrap.min.css",
-                      "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Assets/css/bundles/bootstrap").Include(
+                      "~/Assets/css/bootstrap.min.css"
+                      ));
         }
     }
 }
