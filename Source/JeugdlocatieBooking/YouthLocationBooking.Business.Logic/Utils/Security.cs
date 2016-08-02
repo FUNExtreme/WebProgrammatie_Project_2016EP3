72745122bde9f650ab4994494855e34a53980afc
FUNExtreme <robin.maenhaut.coder@gmail.com>
2016-08-02 21:36:43 +0200

    Add Security util with SHA256 hashing functionality

---
+++ Source/JeugdlocatieBooking/YouthLocationBooking.Business.Logic/Utils/Security.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace YouthLocationBooking.Business.Logic.Utils
+{
+    public class Security
+    {
+        public static string Hash(string password)
+        {
+            SHA256Managed myhash = new SHA256Managed();
+            byte[] hashValue = System.Text.Encoding.UTF8.GetBytes(password);
+            hashValue = myhash.ComputeHash(hashValue);
+            string hash = "";
+
+            for (int x = 0; x < hashValue.Length; x ++)
+                hash += string.Format("{0:x2}", hashValue[x]);
+
+            return hash;
+        }
+    }
+}
