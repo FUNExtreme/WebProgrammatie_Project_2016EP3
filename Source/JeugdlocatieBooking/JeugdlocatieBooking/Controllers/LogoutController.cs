69c42f9fc587afa0cc82385edded5526a3813236
Robin Maenhaut <roma@adifo.be>
2016-08-03 09:55:50 +0200

    Implement /logout

--- Source/JeugdlocatieBooking/JeugdlocatieBooking/Controllers/LogoutController.cs
+++ Source/JeugdlocatieBooking/JeugdlocatieBooking/Controllers/LogoutController.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.Security;
 
 namespace YouthLocationBooking.Web.Controllers
 {
     public class LogoutController : Controller
     {
-        // GET: Logout
         public ActionResult Index()
         {
-            return View();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
