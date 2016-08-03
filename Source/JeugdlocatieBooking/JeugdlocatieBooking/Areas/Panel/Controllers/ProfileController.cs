2c8e2617952d23b1176b0c8d5c913f2d089ec64c
FUNExtreme <robin.maenhaut.coder@gmail.com>
2016-08-03 21:46:46 +0200

    Implement Profile Edit

--- Source/JeugdlocatieBooking/JeugdlocatieBooking/Areas/Panel/Controllers/ProfileController.cs
+++ Source/JeugdlocatieBooking/JeugdlocatieBooking/Areas/Panel/Controllers/ProfileController.cs
@@ -1,18 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using YouthLocationBooking.Business.Logic.Repositories;
+using YouthLocationBooking.Business.Logic.Utils;
+using YouthLocationBooking.Data.Database.Entities;
+using YouthLocationBooking.Data.Database.Mappings;
+using YouthLocationBooking.Data.Validation.Models;
 
 namespace YouthLocationBooking.Web.Areas.Panel.Controllers
 {
     [Authorize]
     public class ProfileController : Controller
     {
-        // GET: Panel/Profile
+        #region Variables
+        private UsersRepository _usersRepository;
+        #endregion
+
+        #region Constructor
+        public ProfileController()
+        {
+            _usersRepository = new UsersRepository();
+        }
+        #endregion
+
+        #region Index
         public ActionResult Index()
         {
-            return View();
+            return RedirectToActionPermanent("Edit", "Profile");
         }
+        #endregion
+
+        #region Edit
+        public ActionResult Edit()
+        {
+            DbUser user = _usersRepository.GetByEmail(User.Identity.Name);
+            ProfileEditValidationModel validationModel = user.ToProfileEditValidationModel();
+            return View(validationModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(ProfileEditValidationModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            // Verify that this new email address is still available
+            if (User.Identity.Name.ToLower() != model.Email.ToLower())
+            {
+                DbUser newEmailUser = _usersRepository.GetByEmail(model.Email);
+                if(newEmailUser != null)
+                {
+                    ModelState.AddModelError(string.Empty, "Email address is already in use, please use a different one");
+                    ModelState.AddModelError("Email", "Already in use");
+                    return View(model);
+                }
+            }
+
+            DbUser user = _usersRepository.GetByEmail(User.Identity.Name);
+            user.FirstName = model.FirstName;
+            user.LastName = model.LastName;
+            user.Email = model.Email;
+            user.PhoneNumber = model.PhoneNumber;
+            _usersRepository.Update(user);
+
+            FormsAuthentication.SignOut();
+            FormsAuthentication.SetAuthCookie(model.Email, true);
+
+            // TODO add success message
+            return View(model);
+        }
+        #endregion
+
+        #region Dispose
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_usersRepository != null)
+                {
+                    _usersRepository.Dispose();
+                    _usersRepository = null;
+                }
+            }
+            base.Dispose(disposing);
+        }
+        #endregion
     }
 }
---
+++ Source/JeugdlocatieBooking/YouthLocationBooking.Models/Validation/Models/ProfileEditValidationModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YouthLocationBooking.Data.Validation.Models
+{
+    public class ProfileEditValidationModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+    }
+}
