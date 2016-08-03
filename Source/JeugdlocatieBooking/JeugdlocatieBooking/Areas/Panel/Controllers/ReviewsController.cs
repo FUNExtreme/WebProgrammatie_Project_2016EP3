b6916640363b69b4a4def20a507a7a09b8a55232
Robin Maenhaut <roma@adifo.be>
2016-08-03 15:10:16 +0200

    Create placeholder Index pages for Panel Controllers

--- Source/JeugdlocatieBooking/JeugdlocatieBooking/Areas/Panel/Controllers/ReviewsController.cs
+++ Source/JeugdlocatieBooking/JeugdlocatieBooking/Areas/Panel/Controllers/ReviewsController.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace YouthLocationBooking.Web.Areas.Panel.Controllers
 {
     [Authorize]
-    public class RatingsController : Controller
+    public class ReviewsController : Controller
     {
-        // GET: Panel/Ratings
         public ActionResult Index()
         {
             return View();
