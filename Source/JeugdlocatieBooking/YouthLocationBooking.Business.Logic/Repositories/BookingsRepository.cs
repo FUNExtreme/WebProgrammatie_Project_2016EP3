17272e6b24b3c096d66f015dad1eb828c9e3d802
Robin Maenhaut <roma@adifo.be>
2016-08-03 14:35:24 +0200

    Create BookingsRepository

---
+++ Source/JeugdlocatieBooking/YouthLocationBooking.Business.Logic/Repositories/BookingsRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using YouthLocationBooking.Data.Database.Entities;
+
+namespace YouthLocationBooking.Business.Logic.Repositories
+{
+    class BookingsRepository : RepositoryBase, IRepository<DbBooking>
+    {
+        public void Add(DbBooking entity)
+        {
+            _dbContext.Bookings.Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public DbBooking Get(int id)
+        {
+            return _dbContext.Bookings.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public IList<DbBooking> GetAllByUserId(int userId)
+        {
+            return _dbContext.Bookings.Where(x => x.UserId == userId).ToList();
+        }
+
+        public IList<DbBooking> GetAll()
+        {
+            return _dbContext.Bookings.ToList();
+        }
+
+        public void Remove(DbBooking entity)
+        {
+            _dbContext.Bookings.Remove(entity);
+            _dbContext.SaveChanges();
+        }
+    }
+}
