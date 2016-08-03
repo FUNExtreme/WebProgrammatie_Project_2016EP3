6e916f404c14d5d1f93045815afe669437dc209d
Robin Maenhaut <roma@adifo.be>
2016-08-03 13:50:23 +0200

    Implement Contact form, performs validation but does not actually send the email (doesn't function properly on the Odisee network)

--- Source/JeugdlocatieBooking/JeugdlocatieBooking/Controllers/ContactController.cs
+++ Source/JeugdlocatieBooking/JeugdlocatieBooking/Controllers/ContactController.cs
@@ -1,12 +1,22 @@
 ﻿using System.Web.Mvc;
+using YouthLocationBooking.Data.Validation.Models;
 
 namespace YouthLocationBooking.Web.Controllers
 {
     public class ContactController : Controller
     {
-        // GET: Contact
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(ContactFormValidationModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
             return View();
         }
     }
