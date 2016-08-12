b36924e2787c91d57cd7b0afcbeb7f1331371bf0
FUNExtreme <robin.maenhaut.coder@gmail.com>
2016-08-12 23:19:46 +0200

    Add Panel Summary page

--- Source/JeugdlocatieBooking/JeugdlocatieBooking/Areas/Panel/Controllers/SummaryController.cs
+++ Source/JeugdlocatieBooking/JeugdlocatieBooking/Areas/Panel/Controllers/SummaryController.cs
@@ -1,13 +1,17 @@
 ﻿using System.Web.Mvc;
+using YouthLocationBooking.Web.Code;
 using YouthLocationBooking.Web.Code.Auth;
 
 namespace YouthLocationBooking.Web.Areas.Panel.Controllers
 {
     [YLBAuthenticate]
-    public class SummaryController : Controller
+    public class SummaryController : UnitOfWorkControllerBase
     {
         public ActionResult Index()
         {
+            var bookingsRepository = _unitOfWork.BookingsRepository;
+            ViewBag.Bookings = bookingsRepository.GetAllByUserId(((AuthenticatedUser)User).Id);
+
             return View();
         }
     }
